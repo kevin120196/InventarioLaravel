d785cbc5d31b0c1bc4bb6745999f2dcdbfa85ec7
kevin120196 <kevintalaverac120196@gmail.com>
2019-11-03 22:08:25 -0600

    Implementacion de atributos a modelos: categorias, clientes, productos, proforma y vendedor

--- app/Cliente.php
+++ app/Cliente.php
@@ -7,4 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Cliente extends Model
 {
     //
+    protected $table="clientes";
+    protected $fillable=['nombre','direccion','cedula','numero_telefono','correo_electronico','descuento_id'];
 }

d024f27b30dab4c6368cbf47c626f9cae1d9ca7e
pisadita <levi.espinal@gmail.com>
2019-11-10 12:33:56 -0300

    elimine estado factura y tipo factura ya que los colocastes como campos enum y esta mejor

--- app/Detalle_Factura_Compra.php
+++ app/Detalle_Factura_Compra.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Detalle_Factura_Compra extends Model
 {
-    //
+    protected $table="detalles_facturas_compras";
+    protected $fillable=['fecha','cantidad','precio','total','facturas_compras_id',
+    'facturas_compras_estados_id','facturas_compras_tipos_id','facturas_compras_proveedor_id',
+    'productos_id_productos','productos_categorias_productos_id','productos_proveedores_id'];
 }
--- database/migrations/2019_11_04_223813_create_facturas_ventas_table.php
+++ database/migrations/2019_11_04_223813_create_facturas_ventas_table.php
@@ -16,17 +16,17 @@ class CreateFacturasVentasTable extends Migration
         Schema::create('facturas_ventas', function (Blueprint $table) {
             $table->increments('id');
             $table->date('fecha_factura');
-         //   $table->enum('tipos_factura',['Credito','Contado']);
-         //   $table->enum('estado_factura',['Cancelada','Pendiente']);
+            $table->enum('tipos_factura',['Credito','Contado']);
+            $table->enum('estado_factura',['Cancelada','Pendiente']);
             //claves foraneas
-            $table->integer('estado_id')->unsigned();
-            $table->integer('tipos_factura_id')->unsigned();
+           // $table->integer('estado_id')->unsigned();
+           // $table->integer('tipos_factura_id')->unsigned();
             $table->integer('clientes_id')->unsigned();
             $table->integer('descuentos_clientes_id')->unsigned();
             $table->integer('vendedores_id')->unsigned();
             //Referencias claves foraneas
-            $table->foreign('estado_id')->references('id')->on('estados_facturas')->onDelete('cascade');
-            $table->foreign('tipos_factura_id')->references('id')->on('tipos_facturas')->onDelete('cascade');
+           // $table->foreign('estado_id')->references('id')->on('estados_facturas')->onDelete('cascade');
+            //$table->foreign('tipos_factura_id')->references('id')->on('tipos_facturas')->onDelete('cascade');
             $table->foreign('clientes_id')->references('id')->on('clientes')->onDelete('cascade');
             $table->foreign('descuentos_clientes_id')->references('id')->on('descuentos_clientes')->onDelete('cascade');
             $table->foreign('vendedores_id')->references('id')->on('vendedores')->onDelete('cascade');

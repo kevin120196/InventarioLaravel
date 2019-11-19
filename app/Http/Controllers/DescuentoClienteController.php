a7129f9e0f03aa89315347414b511e5c5449b111
pisadita <levi.espinal@gmail.com>
2019-11-18 21:38:08 -0300

    descuentocliente, marca controller

--- app/Http/Controllers/DescuentoClienteController.php
+++ app/Http/Controllers/DescuentoClienteController.php
@@ -1,10 +1,47 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use app\Descuento_cliente;
 use Illuminate\Http\Request;
 
 class DescuentoClienteController extends Controller
 {
-    //
+    public function index(){
+        $descuento_cliente=Descuento_Cliente::orderBy('id','ASC')->paginate(3);
+        return view('admin.descuentos_clientes.index')->with('descuento_cliente',$descuento_cliente);
+        
+    }
+
+    public function create(){
+        return view('admin.descuentos_clientes.create');
+
+    }
+
+    public function store(Request $reques){
+        $descuento_cliente=new Descuento_Cliente($reques->all());
+        $descuento_cliente->save();
+        Alert::success('Exito!','El descuento cliente ' . $descuento_cliente->nombre_marca . ' ha sido creada con exito' );
+        //flash('La categoria' . $categoria->nombre_categoria . ' ha sido creada con exito', 'success');
+        return redirect()->route('descuentos_clientes.index');
+    }
+
+    public function edit($id){
+        $descuento_cliente=Descuento_Cliente::find($id);
+        return view('admin.descuentos_clientes.edit')->with('descuento_cliente',$descuento_cliente);
+    }
+
+    public function update(Request $request,$id){
+        $descuento_cliente=Descuento_Cliente::find($id);
+        $descuento_cliente->fill($request->all());
+        $descuento_cliente->save();
+        Alert::success('Exito!','El descuento cliente ' .$descuento_cliente->descuento_cliente. ' ha sido actualizado');
+        return redirect()->route('descuentos_clientes.index');
+    }
+
+    public function destroy($id){
+        $descuento_cliente=Descuento_Cliente::find($id);
+        $descuento_cliente->delete();
+        Alert::error('Eliminado!', 'El descuento cliente ' .$descuento_cliente->descuento_cliente. ' ha sido eliminada');
+        return redirect()->route('descuentos_clientes.index');
+    }
 }
--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -1,10 +1,47 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use app\Marca;
 use Illuminate\Http\Request;
 
 class MarcaController extends Controller
 {
-    //
+    public function index(){
+        $marca=Marca::orderBy('id','ASC')->paginate(3);
+        return view('admin.marca.index')->with('marca',$marca);
+        
+    }
+
+    public function create(){
+        return view('admin.marca.create');
+
+    }
+
+    public function store(Request $reques){
+        $marca=new Marca($reques->all());
+        $marca->save();
+        Alert::success('Exito!','La marca ' . $marca->nombre_marca . ' ha sido creada con exito' );
+        //flash('La categoria' . $categoria->nombre_categoria . ' ha sido creada con exito', 'success');
+        return redirect()->route('marca.index');
+    }
+
+    public function edit($id){
+        $marca=Marca::find($id);
+        return view('admin.marca.edit')->with('marca',$marca);
+    }
+
+    public function update(Request $request,$id){
+        $marca=Marca::find($id);
+        $marca->fill($request->all());
+        $marca->save();
+        Alert::success('Exito!','La marca ' .$marca->nombre_marca. ' ha sido actualizado');
+        return redirect()->route('marca.index');
+    }
+
+    public function destroy($id){
+        $marca=Marca::find($id);
+        $marca->delete();
+        Alert::error('Eliminado!', 'La categoria ' .$marca->nombre_marca. ' ha sido eliminada');
+        return redirect()->route('categorias.index');
+    }
 }

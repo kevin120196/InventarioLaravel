27e9c1d37746aae3fb066243ab6e05f0f4ac2f8e
kevin120196 <kevintalaverac120196@gmail.com>
2019-11-19 21:01:04 -0600

    aceptar cambios

--- resources/views/admin/proveedor/create.blade.php
+++ resources/views/admin/proveedor/create.blade.php
@@ -10,7 +10,7 @@
             {!! Form::text('nombre_proveedor', null, ['placeholder'=>'Nombre Proveedor'])!!}
         </div>
 
-        <div class="input-40 input-contenedor input-100">
+        <div class="input-contenedor input-100">
             <i class="fa fa-id-card icon" aria-hidden="true"></i>
             {!! Form::text('cedula', null, ['placeholder'=>'Numero Cedula','pattern'=>'^([0-9]{3})[ -]([0-9]{6})[ -]([0-9|A-Z]{5})$','title'=>'El formato es: 000-000000-0000L','required'])!!}
         </div>
